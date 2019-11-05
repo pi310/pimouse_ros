9741e87baba12bd01900a5eee9b44459afa5e322
pi310 <s1611130@st.tohtech.ac.jp>
2019-11-05 05:46:39 +0000

    test

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -17,16 +17,16 @@ class LightsensorTest(unittest.TestCase):
 
     def check_values(self,lf,ls,rs,rf):
         vs = self.values
-        self.assertEqual(vs.left_forward, lf "different value: left_forwrad")
-        self.assertEqual(vs.left_side, ls "different value: left_side")
-        self.assertEqual(vs.right_side, rs "different value: rihgt_side")
-        self.assertEqual(vs.right_forward, rf "different value: right_forward")
+        self.assertEqual(vs.left_forward, lf, "different value: left_forwrad")
+        self.assertEqual(vs.left_side, ls, "different value: left_side")
+        self.assertEqual(vs.right_side, rs, "different value: rihgt_side")
+        self.assertEqual(vs.right_forward, rf, "different value: right_forward")
         self.assertEqual(vs.sum_all, lf+ls+rs+rf, "different value: sum_all")
         self.assertEqual(vs.sum_forward, lf+rf, "different value: sum_forward")
 
     def test_node_exist(self):
         nodes = rosnode.get_node_names()
-        self.assertIn('/lightsensors',node. "node does not exist")
+        self.assertIn('/lightsensors',node, "node does not exist")
 
     def test_get_value(self):
         rospy.set_param('lightsensors_freq',10)
