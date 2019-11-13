b399914e54e92f86f980bfe469fbfaa826280cb7
pi310 <s1611130@st.tohtech.ac.jp>
2019-11-13 07:21:00 +0000

    test

--- travis_test_lightsensors.py
+++ travis_test_lightsensors.py
@@ -32,7 +32,7 @@ class LightsensorTest(unittest.TestCase):
         rospy.set_param('lightsensors_freq',10)
         time.sleep(2)
         with open("/dev/rtlightsensor0","w") as f:
-            f.write("-1 0 123 4321 \n")
+            f.write("-1 0 123 4321\n")
 
         time.sleep(3)
 
